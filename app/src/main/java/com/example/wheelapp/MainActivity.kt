dfb2e06e490e429cafc1f18dd1fa2acb0165981f
eevvaaa <eva.rgnl@gmail.com>
2023-02-23 12:37:26 +0100

    test

---
+++ app/src/main/java/com/example/wheelapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.wheelapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val test1 =2
+        //hello
+
+    }
+}
