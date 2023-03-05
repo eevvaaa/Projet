1cb25ed4a3b430e209a797ea9699f9019f0eac79
solene-lem <116102718+solene-lem@users.noreply.github.com>
2023-03-05 10:08:13 +0000

    v0.2

--- FirstTestIOHaptic/src/main.cpp
+++ FirstTestIOHaptic/src/main.cpp
@@ -11,8 +11,11 @@ This module has 3 pins:
     IN: Input
 */
 
-/* On définit une constante In qui sera le numéro sur lequel on branche notre module */
-#define In 1
+/* On définit une constante MotorPin qui sera le numéro sur lequel on branche notre module
+et sensorPin pour le port des sensors */
+#define motorPin 1
+#define sensorPin 2
+
 
 /* On définit nos limites de vibrations */
 #define LIMITE_DANGER 30
@@ -22,16 +25,27 @@ This module has 3 pins:
 void setup() {
 
   Serial.begin(9600); /* On ouvre le 1er port série Arduino, et fixe la vitesse de communication avec l'ordinateur à 9600 caractères par minute */
-  pinMode(In, OUTPUT); /* On définit In comme la sortie --> là où on veut que la arduino envoie les instructions */
-
+  pinMode(motorPin, OUTPUT); /* On définit In comme la sortie --> là où on veut que la arduino envoie les instructions */
+  pinMode(sensorPin, INPUT); /* sensorPIn = INPUT pour la arduino */
 }
 
 /* boucle LOOP qui se répète sans discontinuité */
 void loop() {
   
-  digitalWrite(In, HIGH); /* On allume la vibration */
-  delay(500); /* temps de pause en ms */
-  digitalWrite(In, LOW); /* On éteint la vibration */
-  delay(800);
+  /* peut-être en faire une fonction pour chaque sensor ? */
+  
+  int valeurSensors = analogRead(sensorPin); /* On lit la valeur du sensor */
+
+  if (valeurSensors<=LIMITE_DANGER){ /* Si la valeur est inféreure ou égale à 30 cm, on met le capteur à fond */
+    digitalWrite(motorPin, HIGH);
+    delay(500); /* temps de pause en ms */
+  }else if (valeurSensors>LIMITE_DANGER && valeurSensors<=LIMITE_WARNING){ /* Si la valeur est dans la warning zone, on met le capteur à moitié */
+    digitalWrite(motorPin, 0.5); 
+    delay(500);
+  }else{ /* Sinon, on éteint la vibration */
+    digitalWrite(motorPin, LOW); 
+    delay(500);
+  }
+  
   
 }
